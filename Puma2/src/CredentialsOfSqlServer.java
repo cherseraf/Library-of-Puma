9820ee0b0ef4bb65c2f5d1412612ea2ac5dd536e
cherseraf <152210140+cherseraf@users.noreply.github.com>
2024-02-12 15:00:38 +0300

    Delete Puma2 directory

--- Puma2/src/CredentialsOfSqlServer.java
+++
@@ -1,7 +0,0 @@
-
-public class CredentialsOfSqlServer {
-	
-    static final String DB_URL = "jdbc:mysql://localhost:3306/puma";
-    static final String USER = "root";
-    static final String PASS = "123456";
-}
